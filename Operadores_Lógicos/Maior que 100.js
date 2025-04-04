06588eff7eb194ba8af87ecae685d67d4b78ec56
Cauã Tupinambá <cauatupinamba@gmail.com>
2025-04-04 08:18:06 -0300

    feat(operadores-logicos): adiciona verificação de número maior que 100

---
+++ Operadores_Lógicos/Maior que 100.js
@@ -0,0 +1,19 @@
+let num1 = 1000
+let num2 = 100
+let num3= 10
+
+if (num1 <= 100){
+    console.log("não passou de 100")
+}else{(num1 > 100)
+    console.log("passou de 100")
+    }
+if (num2 <= 100){
+    console.log("não passou de 100")
+}else{ (num2 > 100)
+    console.log("passou de 100")
+    }
+if (num3 <= 100){
+    console.log("não passou de 100")
+}else{ (num3 > 100)
+    console.log("passou de 100")
+    }
